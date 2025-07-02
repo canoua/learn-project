cbd314b7cc7d39206a3962a6a62cd115d84e091d
akaryaka <a.karyaka.2dit@gmail.com>
2025-07-02 20:42:54 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <img v-bind:src="attr"> 
+  <button v-on:click="show">{{ btnText }}</button>
+  <button @click="showCape">showCape</button>
+  <br>
+  <p>{{ changeText }}</p>
+  <button @click="change">change</button>
+  <p>{{ price }}</p>
+  <button @click="changeCost">changeCost</button>
+  <a @click.once="blockClick">modified link</a>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        textCape: 'cape',
+        btnText: 'посмотреть время',
+        attr: 'img.png',
+        dateNow: new Date(),
+        changeText: 'changeText',
+        cost: 400,
+        amount: 5
+      }
+    },
+    methods: {
+      show: function() {
+        alert(this.dateNow);
+      },
+      showCape: function() {
+        alert(this.cape(this.textCape));
+      },
+      cape: function(str) {
+	      return str[0].toUpperCase() + str.slice(1);
+      },
+      change: function() {
+        this.changeText = 'textDoneChange'
+      },
+      changeCost: function() {
+        this.cost = 300;
+      },
+      blockClick: function() {
+        alert('block');
+      }
+    },
+    computed: {
+      price: function() {
+        return this.amount * this.cost;
+      }
+    }
+  }
+</script>
+
+
