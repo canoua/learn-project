8c73fb0fe4ac4ddbe3a5d44c001255c936f59921
akaryaka <akariaka@yandex.ru>
2025-07-09 00:11:30 +0600

    forms done

--- src/components/Forms/Forms_3.vue
+++ src/components/Forms/Forms_3.vue
@@ -2,9 +2,46 @@
   <h2>Форма для добавления элементов массива в Vue</h2>
   <input type="text" v-model="newItem">
   <button @click="addTask">добавить дело</button>
-  <ul v-for="item in items">
-    <li>{{ item }}</li>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      {{ item }}
+      <button @click="removeTask">delete</button>
+    </li>
   </ul>
+
+  <div>
+    <h2>Кнопка для удаления из массива объектов в Vue</h2>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+        {{ user.surn }}
+        <button @click="removeItem(user.id)">remove</button>
+      </li>
+    </ul>
+  </div>
+
+  <div>
+    <h2>Форма для редактирования массива объектов в Vue</h2>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <template v-if="!user.isEdit">
+          {{ user.name }}
+          {{ user.surn }}
+          <button @click="edit(user)">
+            edit
+          </button>
+        </template>
+        <template v-else>
+          <input v-model="user.name">
+          <input v-model="user.surn">
+          <button @click="save(user)">
+            save
+          </button>
+        </template>
+		  </li>
+	</ul>
+  </div>
+
 </template>
 
 <script>
@@ -12,14 +49,64 @@
     data() {
       return {
         newItem: '',
-        items: ['abc', 'cde', 'fgh']
+        items: ['abc', 'cde', 'fgh'],
+        users: [
+          {
+            id: 1,
+            name: 'name1',
+            surn: 'surn1',
+          },
+          {
+            id: 2,
+            name: 'name2',
+            surn: 'surn2',
+          },
+          {
+            id: 3,
+            name: 'name3',
+            surn: 'surn3',
+          },
+        ],
+        users2: [
+          {
+            id: 1,
+            name: 'name1',
+            surn: 'surn1',
+            isEdit: false,
+          },
+          {
+            id: 2,
+            name: 'name2',
+            surn: 'surn2',
+            isEdit: false,
+          },
+          {
+            id: 3,
+            name: 'name3',
+            surn: 'surn3',
+            isEdit: false,
+          },
+        ]
       }
     },
     methods: {
       addTask: function() {
         this.items.unshift(this.newItem);
-        // alert(this.items);
-      }
+      },
+      removeTask: function(index) {
+        this.items.splice(index, 1);
+      },
+      removeItem: function(id) {
+        this.users = this.users.filter((user) => {
+          return user.id !== id;
+        })
+      },
+      edit(user) {
+        user.isEdit = true;
+      },
+      save(user) {
+        user.isEdit = false;
+      },
     }
   }
 </script>
